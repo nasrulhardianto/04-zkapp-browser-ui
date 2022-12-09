d44025510338a06a39cfa98c590d4cbe0859f8e7
nasrulhardianto <kiplikoplak11@gmail.com>
2022-12-09 17:24:19 +0700

    Deploy gh-pages

--- _next/static/chunks/webpack-8691896c2f2313a8.js
+++ _next/static/chunks/webpack-8691896c2f2313a8.js
@@ -121,7 +121,7 @@
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/css/" + "a4f17aff966d594c" + ".css";
+/******/ 			return "static/css/" + "d5e2862de744d495" + ".css";
 /******/ 		};
 /******/ 	}();
 /******/ 	
